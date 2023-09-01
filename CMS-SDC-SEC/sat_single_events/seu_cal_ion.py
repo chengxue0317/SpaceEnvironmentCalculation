9f777312d73ccfe23ba94517823b4cbf0cc77fbe
yuechao <yuechao@piesat.cn>
2023-09-01 09:32:09 +0800

    update

--- CMS-SDC-SEC/sat_single_events/seu_cal_ion.py
+++ CMS-SDC-SEC/sat_single_events/seu_cal_ion.py
@@ -2,120 +2,143 @@ import os
 import sys
 import numpy as np
 import math
+import json
+
 
 # 保留有效数字
 def valid_numbers(x):
-	x = float(np.format_float_positional(x, precision=3, unique = False, fractional = False, trim = 'k'))
-	return x
+    x = float(np.format_float_positional(x, precision=3, unique=False, fractional=False, trim='k'))
+    return x
 
 
-def Cal_Weibull(L,sigma0,L_th,W,S):
-	sigma = np.zeros(len(L))
-	for i in range(len(L)):
-		if L[i]-L_th>0:
-			b = -1*pow((L[i]-L_th)/W,S)
-			sigma[i] = sigma0*(1 - math.exp(b))
-		else:
-			sigma[i] = 0
-	return sigma
+def Cal_Weibull(L, sigma0, L_th, W, S):
+    sigma = np.zeros(len(L))
+    for i in range(len(L)):
+        if L[i]-L_th > 0:
+            b = -1*pow((L[i]-L_th)/W, S)
+            sigma[i] = sigma0*(1 - math.exp(b))
+        else:
+            sigma[i] = 0
+    return sigma
 
-def Read_let_flux(scenario,shield_depth):
-	current_dir = os.path.dirname(os.path.abspath(__file__))
-	f = open(current_dir+'/data_heavy_ion/'+scenario+'_'+shield_depth+'mm_let.let.tsv')
-	size = len(f.readlines())
-	f = open(current_dir+'/data_heavy_ion/'+scenario+'_'+shield_depth+'mm_let.let.tsv')
-	#print(current_dir+'/data_heavy_ion/'+scenario+'_'+shield_depth+'mm_let.let.tsv')
-	LET = []
-	flux = []
-	for i in range(size):
-		txt = f.readline().split()
-		if i>0:
-			LET.append(txt[0])
-			flux.append(txt[1])
-	LET = list(map(float,LET))
-	flux = list(map(float,flux))
-	LET = np.array(LET)
-	flux = np.array(flux)*24*3600/1e4*4*3.14159
-	return LET/1000,flux  # let: MeV cm2/mg; flux: m2 s-sr
 
-def Cal_effective_flux(LET,flux):
-	# Analytic SEU rate calculation compared to space data, Publisher: IEEE
-	flux_e = np.zeros(len(LET))
-	for i in range(len(LET)):
-		if LET[i]<L_th:
-			flux_e[i] = 0.5*flux[i]*pow(LET[i]/L_th,2)
-		else:
-			flux_e[i] = 0.5*flux[i]
-	return flux_e
+def Read_let_flux(scenario, shield_depth):
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    f = open(current_dir+'/data_heavy_ion/'+scenario+'_'+shield_depth+'mm_let.let.tsv')
+    size = len(f.readlines())
+    f = open(current_dir+'/data_heavy_ion/'+scenario+'_'+shield_depth+'mm_let.let.tsv')
+    # print(current_dir+'/data_heavy_ion/'+scenario+'_'+shield_depth+'mm_let.let.tsv')
+    LET = []
+    flux = []
+    for i in range(size):
+        txt = f.readline().split()
+        if i > 0:
+            LET.append(txt[0])
+            flux.append(txt[1])
+    LET = list(map(float, LET))
+    flux = list(map(float, flux))
+    LET = np.array(LET)
+    flux = np.array(flux)*24*3600/1e4*4*3.14159
+    return LET/1000, flux  # let: MeV cm2/mg; flux: m2 s-sr
+
+
+def Cal_effective_flux(LET, flux):
+    # Analytic SEU rate calculation compared to space data, Publisher: IEEE
+    flux_e = np.zeros(len(LET))
+    for i in range(len(LET)):
+        if LET[i] < L_th:
+            flux_e[i] = 0.5*flux[i]*pow(LET[i]/L_th, 2)
+        else:
+            flux_e[i] = 0.5*flux[i]
+    return flux_e
 
 
 # 参数
 para_switch = int(sys.argv[1])
 if para_switch == 1:
-	Weibull_para_name = str(sys.argv[2])
+    Weibull_para_name = str(sys.argv[2])
 
+total_SEU = []
+dic = {}
+for scenario in range(1, 6):
+    if scenario == 1:
+        scenario = 'solar_min'
+    elif scenario == 2:
+        scenario = 'solar_max'
+    elif scenario == 3:
+        scenario = 'worst_week'
+    elif scenario == 4:
+        scenario = 'worst_day'
+    elif scenario == 5:
+        scenario = 'peak_5min'
+    
+    depth = ['1', '3', '5']
+    for shield_depth in depth:
+        if para_switch == 1:
+            current_dir = os.path.dirname(os.path.abspath(__file__))
+            para_name = []
+            para_sigma0 = []
+            para_Lth = []
+            para_W = []
+            para_S = []
+            f = open(current_dir+'/Weibull_parameter.txt')
+            size = len(f.readlines())
+            f = open(current_dir+'/Weibull_parameter.txt')
+            for i in range(size):
+                txt = f.readline().split()
+                if i > 0:
+                    para_name.append(txt[0])
+                    para_sigma0.append(txt[1])
+                    para_Lth.append(txt[2])
+                    para_W.append(txt[3])
+                    para_S.append(txt[4])
 
-for scenario in range(1,6):
-	if scenario==1:
-		scenario = 'solar_min'
-	elif scenario==2:
-		scenario = 'solar_max'
-	elif scenario==3:
-		scenario = 'worst_week'
-	elif scenario==4:
-		scenario = 'worst_day'
-	elif scenario ==5:
-		scenario = 'peak_5min'
-	
-	depth = ['1','3','5']
-	for shield_depth in depth:
-		if para_switch == 1:
-			current_dir = os.path.dirname(os.path.abspath(__file__))
-			para_name = []
-			para_sigma0 = []
-			para_Lth = []
-			para_W = []
-			para_S = []
-			f = open(current_dir+'/Weibull_parameter.txt')
-			size = len(f.readlines())
-			f = open(current_dir+'/Weibull_parameter.txt')
-			for i in range(size):
-				txt = f.readline().split()
-				if i >0:
-					para_name.append(txt[0])
-					para_sigma0.append(txt[1])
-					para_Lth.append(txt[2])
-					para_W.append(txt[3])
-					para_S.append(txt[4])
+            sigma0 = float(para_sigma0[para_name.index(Weibull_para_name)])
+            L_th = float(para_Lth[para_name.index(Weibull_para_name)])
+            W = float(para_W[para_name.index(Weibull_para_name)])
+            S = float(para_S[para_name.index(Weibull_para_name)])
+        elif para_switch == 2:
+            sigma0 = float(sys.argv[2])
+            L_th = float(sys.argv[3])
+            W = float(sys.argv[4])
+            S = float(sys.argv[5])
 
-			sigma0 = float(para_sigma0[para_name.index(Weibull_para_name)])
-			L_th = float(para_Lth[para_name.index(Weibull_para_name)])
-			W = float(para_W[para_name.index(Weibull_para_name)])
-			S = float(para_S[para_name.index(Weibull_para_name)])
-		elif para_switch == 2:
-			sigma0 = float(sys.argv[2])
-			L_th = float(sys.argv[3])
-			W = float(sys.argv[4])
-			S = float(sys.argv[5])
+        # Calculate SEU_ion
+        LET, flux = Read_let_flux(scenario, shield_depth)
+        sigma = Cal_Weibull(LET, sigma0, L_th, W, S)
+        flux_e = Cal_effective_flux(LET, flux)
+        SEU = 0
+        for i in range(len(flux_e)):
+            SEU = SEU+sigma[i]*flux_e[i]
+        SEU = valid_numbers(SEU)
+        SEU = format('{:.2e}'.format(SEU, 'e'))
+        total_SEU.append(SEU)
 
-
-		# Calculate SEU_ion
-		LET,flux = Read_let_flux(scenario,shield_depth)
-		sigma = Cal_Weibull(LET,sigma0,L_th,W,S)
-		flux_e = Cal_effective_flux(LET,flux)
-		SEU = 0
-		for i in range(len(flux_e)):
-			SEU = SEU+sigma[i]*flux_e[i]
-		SEU = valid_numbers(SEU)
-		SEU = format('{:.2e}'.format(SEU,'e'))
-		print('#### SEU by heavy ion,',scenario,',',shield_depth,'mm shield depth:',SEU,'bit/day ####')
-print('### parameter ####')
+dic = {}
 if para_switch == 1:
-	print('name: ',Weibull_para_name)
+    dic['material name'] = Weibull_para_name
 elif para_switch == 2:
-	print('name:','自定义器件')
-print('sigma0:',sigma0)
-print('L_th:',L_th)
-print('W:',W)
-print('S:',S)
-print('#################')
+    dic['material name'] = 'user-defined material'
+dic['sigma0'] = sigma0
+dic['LET threshold'] = L_th
+dic['Weibull para W'] = W
+dic['Weibull para S'] = S 
+
+dic['solar min 1mm depth'] = total_SEU[0]
+dic['solar min 3mm depth'] = total_SEU[1]
+dic['solar min 5mm depth'] = total_SEU[2]
+dic['solar max 1mm depth'] = total_SEU[3]
+dic['solar max 3mm depth'] = total_SEU[4]
+dic['solar max 5mm depth'] = total_SEU[5]
+dic['worst week 1mm depth'] = total_SEU[6]
+dic['worst week 3mm depth'] = total_SEU[7]
+dic['worst day 5mm depth'] = total_SEU[8]
+dic['worst day 1mm depth'] = total_SEU[9]
+dic['worst day 3mm depth'] = total_SEU[10]
+dic['worst day 5mm depth'] = total_SEU[11]
+dic['solar peak 1mm depth'] = total_SEU[12]
+dic['solar peak 3mm depth'] = total_SEU[13]
+dic['solar peak 5mm depth'] = total_SEU[14]
+print('######')
+print(json.dumps(dic, ensure_ascii=False))
+print('######')
--- CMS-SDC-SEC/sat_single_events/seu_cal_proton.py
+++ CMS-SDC-SEC/sat_single_events/seu_cal_proton.py
@@ -1,19 +1,11 @@
-import dmPython
-import re
 import os
 import sys
 import math
-import scipy
-import shutil
 import warnings
 import numpy as np
-np.set_printoptions(threshold=sys.maxsize)
-import pandas as pd
-import random
-warnings.filterwarnings("ignore")
 import json
-import time
-
+np.set_printoptions(threshold=sys.maxsize)
+warnings.filterwarnings("ignore")
 '''
 单粒子效应：以单粒子翻转为主。半导体期间的单粒子翻转率是指器件每天
 每bit发生单粒子翻转活错误的概率。计算空间质子产生的单粒子翻转率，
@@ -28,80 +20,82 @@ import time
 
 
 # 计算极限翻转截面：Bendel 双参模型
-def Cal_limiting_cross_section_Bendel2(E,A,B):
-	Cross = np.zeros(len(E))
-	for i in range(0,len(E)):
-		if (E[i]-A)>0:
-			Y = math.sqrt(18/A)*(E[i]-A)
-			Y = -0.18*math.sqrt(Y)
-			Y = pow(1-math.exp(Y),4)
-			Y = pow(B/A,14)*Y*1e-12
-			Cross[i] = Y
-		else:
-			Cross[i] = 0
-	return Cross
+def Cal_Bendel2(E, A, B):
+    Cross = np.zeros(len(E))
+    for i in range(0, len(E)):
+        if (E[i]-A) > 0:
+            Y = math.sqrt(18/A)*(E[i]-A)
+            Y = -0.18*math.sqrt(Y)
+            Y = pow(1-math.exp(Y), 4)
+            Y = pow(B/A, 14)*Y*1e-12
+            Cross[i] = Y
+        else:
+            Cross[i] = 0
+    return Cross
 
 
 # 计算极限翻转截面：Bendel 单参模型
-def Cal_limiting_cross_section_Bendel1(E,A):
-	Cross = np.zeros(len(E))
-	for i in range(0,len(E)):
-		if (E[i]-A)>0:
-			Y = math.sqrt(18/A)*(E[i]-A)
-			Y = -0.18*math.sqrt(Y)
-			Y = pow(1-math.exp(Y),4)
-			Y = pow(24/A,14)*Y*1e-12
-			Cross[i] = Y
-		else:
-			Cross[i] = 0
-	return Cross
+def Cal_Bendel1(E, A):
+    Cross = np.zeros(len(E))
+    for i in range(0, len(E)):
+        if (E[i]-A) > 0:
+            Y = math.sqrt(18/A)*(E[i]-A)
+            Y = -0.18*math.sqrt(Y)
+            Y = pow(1-math.exp(Y), 4)
+            Y = pow(24/A, 14)*Y*1e-12
+            Cross[i] = Y
+        else:
+            Cross[i] = 0
+    return Cross
 
 
 # 根据极限翻转截面计算单粒子翻转率
-def Cal_SEU_proton(Cross,flux):
-	s = 0
-	for i in range(len(flux)):
-		s = s+Cross[i]*flux[i]
-	return s
+def Cal_SEU_proton(Cross, flux):
+    s = 0
+    for i in range(len(flux)):
+        s = s+Cross[i]*flux[i]
+    return s
 
 
 # 读取空间环境辐射数据--低轨卫星质子通量
-def Cal_radiation_flux(shield_depth,solar,inc,height):
-	current_dir = os.path.dirname(os.path.abspath(__file__))
-	file_path = current_dir + '/data_proton/'
-	file_name = 'trans'+str(shield_depth)+'mm_'+str(solar)+'_inc_'+str(inc)+'h_'+str(height)+'.tfx.tsv'
-	f = open(file_path+file_name)
-	size = len(f.readlines())
-	f = open(file_path+file_name)
-	E = np.zeros(size)
-	F = np.zeros(size)
-	for i in range(size):
-		txt = f.readline().split()
-		if i>1:
-			E[i] = float(txt[0])
-			F[i] = float(txt[1])*3600*24/10000*4*3.14
-	return E,F
+def Cal_radiation_flux(shield_depth, solar, inc, height):
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = current_dir + '/data_proton/'
+    file_name = 'trans'+str(shield_depth)+'mm_'+str(solar)+'_inc_'+str(inc)+'h_'+str(height)+'.tfx.tsv'
+    f = open(file_path+file_name)
+    size = len(f.readlines())
+    f = open(file_path+file_name)
+    E = np.zeros(size)
+    F = np.zeros(size)
+    for i in range(size):
+        txt = f.readline().split()
+        if i > 1:
+            E[i] = float(txt[0])
+            F[i] = float(txt[1])*3600*24/10000*4*3.14
+    return E, F
+
 
 # 保留有效数字
 def valid_numbers(x):
-	x = float(np.format_float_positional(x, precision=3, unique = False, fractional = False, trim = 'k'))
-	return x
+    x = float(np.format_float_positional(x, precision=3, unique=False, fractional=False, trim='k'))
+    return x
 
-def flux_target_height_inc(shield_depth,solar,i_low,h_low,i_high,h_high):
-	energy_1,flux_1 = Cal_radiation_flux(shield_depth,solar,i_low,h_low)
-	energy_2,flux_2 = Cal_radiation_flux(shield_depth,solar,i_high,h_low)
-	flux_h_low = np.zeros(len(flux_1))
-	for i in range(len(flux_1)):
-		flux_h_low[i] = flux_1[i]+(flux_2[i]-flux_1[i])/2*(i_high-incl)
-	energy_3,flux_3 = Cal_radiation_flux(shield_depth,solar,i_low,h_high)
-	energy_4,flux_4 = Cal_radiation_flux(shield_depth,solar,i_high,h_high)
-	flux_h_high = np.zeros(len(flux_3))
-	for i in range(len(flux_3)):
-		flux_h_high[i] = flux_3[i]+(flux_4[i]-flux_3[i])/2*(i_high-incl)
-	flux = np.zeros(len(flux_1))
-	for i in range(len(flux)):
-		flux[i] = flux_h_low[i]+(flux_h_high[i]-flux_h_low[i])/50*(h_high-geo_height)
-	return flux,energy_1
+
+def flux_target_height_inc(shield_depth, solar, i_low, h_low, i_high, h_high):
+    energy_1, flux_1 = Cal_radiation_flux(shield_depth, solar, i_low, h_low)
+    energy_2, flux_2 = Cal_radiation_flux(shield_depth, solar, i_high, h_low)
+    flux_h_low = np.zeros(len(flux_1))
+    for i in range(len(flux_1)):
+        flux_h_low[i] = flux_1[i]+(flux_2[i]-flux_1[i])/2*(i_high-incl)
+    energy_3, flux_3 = Cal_radiation_flux(shield_depth, solar, i_low, h_high)
+    energy_4, flux_4 = Cal_radiation_flux(shield_depth, solar, i_high, h_high)
+    flux_h_high = np.zeros(len(flux_3))
+    for i in range(len(flux_3)):
+        flux_h_high[i] = flux_3[i]+(flux_4[i]-flux_3[i])/2*(i_high-incl)
+    flux = np.zeros(len(flux_1))
+    for i in range(len(flux)):
+        flux[i] = flux_h_low[i]+(flux_h_high[i]-flux_h_low[i])/50*(h_high-geo_height)
+    return flux, energy_1
 
 
 # 参数
@@ -109,71 +103,88 @@ incl = float(str(sys.argv[1]))   # 倾角
 i_low = int(incl/2)*2
 i_high = i_low+2
 
-perigee = float(str(sys.argv[2])) # 近地点
-apogee = float(str(sys.argv[3])) # 远地点
-geo_height = (perigee+apogee)/2 # 近地点远地点平均
+perigee = float(str(sys.argv[2]))  # 近地点
+apogee = float(str(sys.argv[3]))  # 远地点
+geo_height = (perigee+apogee)/2  # 近地点远地点平均
 h_low = int(geo_height/50)*50
 h_high = h_low+50
 
-Bendel = int(str(sys.argv[4])) # Bendel参数化方案，1：Bendel单参 2：Bendel双参
+Bendel = int(str(sys.argv[4]))  # Bendel参数化方案，1：Bendel单参 2：Bendel双参
 
 para_switch = int(str(sys.argv[5]))
 if para_switch == 1:
-	Bendel_para_name = str(sys.argv[6])
+    Bendel_para_name = str(sys.argv[6])
 
 
-depth = ['1','3','5']
-scenario = ['ap8max','ap8max']
+depth = ['1', '3', '5']
+scenario = ['ap8max', 'ap8max']
+total_SEU = []
 for shield_depth in depth: 
-	for solar in scenario:
-		para_name = []
-		para_A = []
-		flux,E = flux_target_height_inc(shield_depth,solar,i_low,h_low,i_high,h_high)
-		current_dir = os.path.dirname(os.path.abspath(__file__))
-		if Bendel == 1:	
-			if para_switch == 1:
-				f = open(current_dir+'/Bendel1_parameter.txt')
-				size = len(f.readlines())
-				f = open(current_dir+'/Bendel1_parameter.txt')
-				for i in range(size):
-					txt = f.readline().split()
-					if i>0:
-						para_name.append(txt[0])
-						para_A.append(txt[1])
-				A = float(para_A[para_name.index(Bendel_para_name)])
-			elif para_switch == 2:
-				A = float(sys.argv[6])
-			Cross = Cal_limiting_cross_section_Bendel1(E,A)
-			SEU = Cal_SEU_proton(Cross,flux)
+    for solar in scenario:
+        para_name = []
+        para_A = []
+        flux, E = flux_target_height_inc(shield_depth, solar, i_low, h_low, i_high, h_high)
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        if Bendel == 1: 
+            if para_switch == 1:
+                f = open(current_dir+'/Bendel1_parameter.txt')
+                size = len(f.readlines())
+                f = open(current_dir+'/Bendel1_parameter.txt')
+                for i in range(size):
+                    txt = f.readline().split()
+                    if i > 0:
+                        para_name.append(txt[0])
+                        para_A.append(txt[1])
+                A = float(para_A[para_name.index(Bendel_para_name)])
+                para_name = (para_name[para_name.index(Bendel_para_name)])
+            elif para_switch == 2:
+                A = float(sys.argv[6])
+                para_name = 'user-defined material'
+            Cross = Cal_Bendel1(E, A)
+            SEU = Cal_SEU_proton(Cross, flux)
 
-		elif Bendel == 2:
-			if para_switch == 1:
-				para_B = []
-				f = open(current_dir+'/Bendel2_parameter.txt')
-				size = len(f.readlines())
-				f = open(current_dir+'/Bendel2_parameter.txt')
-				for i in range(size):
-					txt = f.readline().split()
-					if i>0:
-						para_name.append(txt[0])
-						para_A.append(txt[1])
-						para_B.append(txt[2])
-				A = float(para_A[para_name.index(Bendel_para_name)])
-				B = float(para_B[para_name.index(Bendel_para_name)])
-			elif para_switch == 2:
-				A = float(sys.argv[6])
-				B = float(sys.argv[7])
-			Cross = Cal_limiting_cross_section_Bendel2(E,A,B)
-			SEU = Cal_SEU_proton(Cross,flux)
-		SEU = valid_numbers(SEU)
-		SEU = format('{:.2e}'.format(SEU,'e'))
-		print('#### SEU by protron,',solar,',',shield_depth,'mm shield depth:',SEU,'bit/day ####' )
-print('### parameter ####')
+        elif Bendel == 2:
+            if para_switch == 1:
+                para_B = []
+                f = open(current_dir+'/Bendel2_parameter.txt')
+                size = len(f.readlines())
+                f = open(current_dir+'/Bendel2_parameter.txt')
+                for i in range(size):
+                    txt = f.readline().split()
+                    if i > 0:
+                        para_name.append(txt[0])
+                        para_A.append(txt[1])
+                        para_B.append(txt[2])
+                A = float(para_A[para_name.index(Bendel_para_name)])
+                B = float(para_B[para_name.index(Bendel_para_name)])
+                para_name = (para_name[para_name.index(Bendel_para_name)])
+            elif para_switch == 2:
+                A = float(sys.argv[6])
+                B = float(sys.argv[7])
+                para_name = 'user-defined material'
+            Cross = Cal_Bendel2(E, A, B)
+            SEU = Cal_SEU_proton(Cross, flux)
+        SEU = valid_numbers(SEU)
+        SEU = format('{:.2e}'.format(SEU, 'e'))
+        total_SEU.append(SEU)
+        
+
+dic = {}
 if Bendel == 1:
-	print('name: Bendel1_para')
-	print('A: ',A)
+    dic['material name'] = para_name
+    dic['Bendel single para A value'] = A
 else:
-	print('name: Bendel2_para')
-	print('A: ',A)
-	print('B: ',B)
-print('#################')
+    dic['material name'] = para_name
+    dic['Bendel double para A value'] = A
+    dic['Bendel double para B value'] = B
+
+dic['solar min 1mm depth'] = total_SEU[0]
+dic['solar min 3mm depth'] = total_SEU[1]
+dic['solar min 5mm depth'] = total_SEU[2]
+dic['solar max 1mm depth'] = total_SEU[3]
+dic['sloar max 3mm depth'] = total_SEU[4]
+dic['solar max 5mm depth'] = total_SEU[5]
+
+print('######')
+print(json.dumps(dic, ensure_ascii=False))
+print('######')
